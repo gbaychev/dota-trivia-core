0b5e95fc6c1d7d615e50432e7708930ab4795339
Georgi Baychev <georgi.baychev@gmail.com>
2017-08-27 23:10:00 +0200

    Remove deliberate exception throwing and fix the core test spec

--- spec/DotaTriviaCoreSpec.js
+++ spec/DotaTriviaCoreSpec.js
@@ -4,14 +4,15 @@ const baseUrl = "http://localhost:3000";
 
 describe("Dota Trivia Core", function(){
     describe("GET /", function() {
-        it("has properly working session", function (){
+        it("has properly working session", function (done){
             request.get(baseUrl, function(error, response, body) {
                 expect(response.statusCode).toBe(200);
-            });
-
-            request.get(baseUrl, function(error, response, body) {
-                expect(response.statusCode).toBe(200);
-                done();
+                
+                // fire second request
+                request.get(baseUrl, function(error, response, body) {
+                    expect(response.statusCode).toBe(200);
+                    done();
+                });
             });
         });
     });
